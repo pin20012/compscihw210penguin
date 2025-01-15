a70bbc407d793205f5a5c334a6ea8c1b17d63d0d
zpinney03 <zpinney03@gmail.com>
2025-01-15 15:42:37 -0700

    guess the number code added

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //gets the magic number
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 11);
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            // gets the user's guess
+            Console.Write("What is your guess? ");
+            string userGuess = Console.ReadLine();
+            guess = int.Parse(userGuess);
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else if (guess == magicNumber)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
