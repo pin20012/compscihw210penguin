6e334aed9bd370c0470a88b12664287892ca363b
zpinney03 <zpinney03@gmail.com>
2025-02-18 17:37:34 -0700

    learning 04 code

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment simpleAssignment = new Assignment("Jonn Doe", "Math");
+        Console.WriteLine(simpleAssignment.GetSummary());
+
+        MathAssignment ma1 = new MathAssignment("Logan Thomas", "Math", "3.4", "4-20 odd");
+        Console.WriteLine(ma1.GetSummary());
+        Console.WriteLine(ma1.GetHomeworkList());
+
+        WritingAssignment wa1 = new WritingAssignment("Steven Stone", "Computer Science", "Learning the principle of Encapsulation");
+        Console.WriteLine(wa1.GetSummary());
+        Console.WriteLine(wa1.GetWritingInformation());
+
     }
 }
