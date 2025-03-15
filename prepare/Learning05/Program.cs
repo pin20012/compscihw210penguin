c149fb759af52453c25db4a79644f499dc0335f3
zpinney03 <zpinney03@gmail.com>
2025-03-14 21:17:13 -0600

    develop 05 start

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -6,25 +6,27 @@ class Program
     {
         List<Shape> shapesList = new List<Shape>();
 
-        Square square1 = new Square("Blue", 5);
+        Square square1 = new Square("Blue", "square", 5);
         shapesList.Add(square1);
 
-        Rectangle rectangle1 = new Rectangle("Yello", 2, 7);
+        Rectangle rectangle1 = new Rectangle("Yellow", "rectangle", 2, 7);
         shapesList.Add(rectangle1);
 
-        Circle circle1 = new Circle("Green", 6);
+        Circle circle1 = new Circle("Green", "circle", 6);
         shapesList.Add(circle1);
 
-        Triangle triangle1 = new Triangle("Purple", 10, 15);
+        Triangle triangle1 = new Triangle("Purple", "triangle", 10, 15);
         shapesList.Add(triangle1);
 
         foreach (Shape s in shapesList)
         {
             string color = s.GetColor();
 
+            string shape = s.GetShape();
+
             double area = s.GetArea();
 
-            Console.WriteLine($"The {color} shape has an area of {area}.");
+            Console.WriteLine($"The {color} {shape} has an area of {area}.");
         }
     }
 }
--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -2,8 +2,70 @@ using System;
 
 class Program
 {
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        string userInput = "";
+
+        int userNumber = -1;
+
+        List<Goal> _assembledGoals = new List<Goal>();
+        List<int> _scores = new List<int>();
+
+        while (userNumber != 6)
+        {
+
+            Console.WriteLine();
+            Console.WriteLine($"You have 0 points");
+            Console.WriteLine();
+            Console.WriteLine("Menu options:");
+            Console.WriteLine("1. Create New Goal");
+            Console.WriteLine("2. List Goals");
+            Console.WriteLine("3. Save Goals");
+            Console.WriteLine("4. Load Goals");
+            Console.WriteLine("5. Record Event");
+            Console.WriteLine("6. Quit");
+            Console.Write("Select a choice from the menu: ");
+            string userAction = Console.ReadLine();
+            userNumber = int.Parse(userAction);
+
+            if (userNumber == 1)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"The Types of goals are: ");
+                Console.WriteLine($"1. Simple Goal");
+                Console.WriteLine($"2. Eternal Goal");
+                Console.WriteLine($"3. Checklist Goal");
+                Console.Write($"Which type of goal would you like to create? ");
+                string userGoaltype = Console.ReadLine();
+                userNumber = int.Parse(userGoaltype);
+
+                if (userNumber == 1)
+                {
+                    Goal g1 = new Goal();
+                    g1.CreateGoal();
+
+                    _assembledGoals.Add(g1);
+                }
+                
+                if (userNumber == 2)
+                {
+                    Console.WriteLine();
+                }
+                if (userNumber == 3)
+                {
+                    Console.WriteLine($"");
+                }
+            }
+
+            if (userNumber == 2)
+            {
+                Console.WriteLine($"The goals are: ");
+                foreach (Goal g in _assembledGoals)
+                {
+                    Console.WriteLine($"goal: {g}");
+                }
+            }
+        }
     }
 }
