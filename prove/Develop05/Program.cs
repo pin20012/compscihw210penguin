fcba9315e1d9dd02f5f1c047c4f637826bb6b1df
zpinney03 <zpinney03@gmail.com>
2025-03-20 20:29:31 -0600

    project outline for foundation 1 and 2

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -25,21 +25,6 @@ class Program
         while (userNumber != 6)
         {
 
-            // Console.WriteLine();
-            // Console.WriteLine($"You have {_totalScore} points");
-            // Console.WriteLine();
-            // Console.WriteLine("Menu options:");
-            // Console.WriteLine("1. Create New Goal");
-            // Console.WriteLine("2. List Goals");
-            // Console.WriteLine("3. Save Goals");
-            // Console.WriteLine("4. Load Goals");
-            // Console.WriteLine("5. Record Event");
-            // Console.WriteLine("6. Quit");
-            // Console.WriteLine();
-            // Console.Write("Select a choice from the menu: ");
-            // string userAction = Console.ReadLine();
-            // userNumber = int.Parse(userAction);
-
            Console.WriteLine();
             Console.WriteLine($"You have {_totalScore} points");
             Console.WriteLine($"Current level: level {_level}");
