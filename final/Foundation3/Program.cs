30253275280491fecc3ce87561bf977ffef4fe32
zpinney03 <zpinney03@gmail.com>
2025-04-04 16:47:55 -0600

    Final Completed

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -5,12 +5,55 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        //Lecture Event
+        Address a1 = new Address("458 Cherry Hill", "Casper", "Wyoming", "81112", "United States");
+        Lecture l1 = new Lecture("Getting an Internship", "The best strategies to get an internship fast", "April 25, 2025", "3:00 PM", a1.GetAddress(), "John Johnson", 215);
+       
+        Console.WriteLine();
+        Console.WriteLine($"========================================");
+        Console.WriteLine($"               Lecture                  ");
+        Console.WriteLine($"========================================");
+        Console.WriteLine($"Full details: ");
+        Console.WriteLine(l1.FullDetails());
+        Console.WriteLine();
+        Console.WriteLine($"Standard details: ");
+        Console.WriteLine(l1.StandardDetails());
+        Console.WriteLine();
+        Console.WriteLine($"Short Description: ");
+        Console.WriteLine(l1.ShortDescription());
 
-        DateTime myDate = new DateTime(2025, 3, 22); // A specific date
-        TimeSpan myTime = new TimeSpan(14, 30, 0);   // A specific time (2:30 PM)
+        //Reception Event
+        Address a2 = new Address("5902 Channing Avenue", "Fort Lauderale", "Florida", "28182", "United States");
+        Reception r1 = new Reception($"VHS End of School Year Banquet", "Verlander High Schol Banquet for Seniors", "May 24, 2025", "6:30 PM", a2.GetAddress(), "vrankor@gmail.com");
+        
+        Console.WriteLine();
+        Console.WriteLine($"========================================");
+        Console.WriteLine($"              Reception                 ");
+        Console.WriteLine($"========================================");
+        Console.WriteLine($"Full details: ");
+        Console.WriteLine(r1.FullDetails());
+        Console.WriteLine();
+        Console.WriteLine($"Standard details: ");
+        Console.WriteLine(r1.StandardDetails());
+        Console.WriteLine();
+        Console.WriteLine($"Short Description: ");
+        Console.WriteLine(r1.ShortDescription());
 
-        Console.WriteLine($"{myDate} {myTime}");
-
+        //Outdoor Event
+        Address a3 = new Address("59 Hemmingway Avenue", "Fort Worth", "Texas", "34241", "United States");
+        Outdoor o1 = new Outdoor($"Fort Worth Farmer's Market", "Many food vendors will be there selling their products", "April 17, 2025", "10 AM - 4 PM", a3.GetAddress(), "Sunny with a high of 82°F");
+        
+        Console.WriteLine();
+        Console.WriteLine($"========================================");
+        Console.WriteLine($"                Outdoor                  ");
+        Console.WriteLine($"========================================");
+        Console.WriteLine($"Full details: ");
+        Console.WriteLine(o1.FullDetails());
+        Console.WriteLine();
+        Console.WriteLine($"Standard details: ");
+        Console.WriteLine(o1.StandardDetails());
+        Console.WriteLine();
+        Console.WriteLine($"Short Description: ");
+        Console.WriteLine(o1.ShortDescription());
     }
 }
--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,7 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> _activities = new List<Activity>();
+
+        Running r1 = new Running("05 Jun 2025", 30.7, 5.6);
+        _activities.Add(r1);
+
+        Cycling c1 = new Cycling("06 Jun 2025", 30.1, 10.5);
+        _activities.Add(c1);
+        
+        Swimming s1 = new Swimming("07 Jun 2025", 30.4, 4.5);
+        _activities.Add(s1);
+
+        foreach (Activity a in _activities)
+        {
+            Console.WriteLine(a.GetSummary());
+        }
     }
 }
 
