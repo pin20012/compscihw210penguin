c4098814a6bd1c62c9b8985ef0296cc3ab386e25
zpinney03 <zpinney03@gmail.com>
2025-04-04 17:13:44 -0600

    updates to Foundation 4

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -6,18 +6,24 @@ class Program
     {
         List<Activity> _activities = new List<Activity>();
 
-        Running r1 = new Running("05 Jun 2025", 30.7, 5.6);
+        Console.WriteLine();
+
+        Running r1 = new Running("05 Jun 2025", 30.7, 5.6, "Running");
         _activities.Add(r1);
 
-        Cycling c1 = new Cycling("06 Jun 2025", 30.1, 10.5);
+        Cycling c1 = new Cycling("06 Jun 2025", 30.1, 10.5, "Cycling");
         _activities.Add(c1);
         
-        Swimming s1 = new Swimming("07 Jun 2025", 30.4, 4.5);
+        Swimming s1 = new Swimming("07 Jun 2025", 30.4, 4.5, "Swimming");
         _activities.Add(s1);
 
         foreach (Activity a in _activities)
         {
+            Console.WriteLine($"========================================");
+            Console.WriteLine($"               {a.ActivityType()}                 ");
+            Console.WriteLine($"========================================");
             Console.WriteLine(a.GetSummary());
+            Console.WriteLine();
         }
     }
 }
